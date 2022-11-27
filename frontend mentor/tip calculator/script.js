7e89c7bf8cec032134322d0e800348e5566590ba
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-11-26 20:04:56 -0500

    started js and fixed html

---
+++ frontend mentor/tip calculator/script.js
@@ -0,0 +1,6 @@
+const billInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".people-input");
+const tipPerPerson = document.querySelector(".bill-input");
+const totalPerPerson = document.querySelector(".bill-input");
+
+billInput.value = "0.0";
