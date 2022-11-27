ba58435244a4398e85b35d70941b076cfbd8d9be
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-11-26 21:01:13 -0500

    more changes

--- frontend mentor/tip calculator/script.js
+++ frontend mentor/tip calculator/script.js
@@ -1,6 +1,55 @@
-const billInput = document.querySelector(".bill-input");
+const billInput = document.querySelector("bill-input");
+const tipBtns = document.getElementsByClassName("button");
+const customTipInput = document.querySelector(".custom");
 const peopleInput = document.querySelector(".people-input");
-const tipPerPerson = document.querySelector(".bill-input");
-const totalPerPerson = document.querySelector(".bill-input");
+// const errorMsg = document.getElementById("error-msg");
 
-billInput.value = "0.0";
+const tipAmountPerPerson = document.getElementById("tip-amount");
+const totalAmountPerPerson = document.getElementById("total-amount");
+console.log(1);
+const resetBtn = document.getElementById("reset");
+
+let tipPercent = 0;
+
+for (let btn of tipBtns) {
+  btn.addEventListener("click", function () {
+    tipPercent = Number.parseInt(btn.textContent);
+  });
+}
+
+peopleInput.addEventListener("input", function () {
+  if (+peopleInput.value === 0) {
+    errorMsg.style.visibility = "visible";
+    tipAmountPerPerson.textContent = `$0.00`;
+    totalAmountPerPerson.textContent = `$0.00`;
+
+    resetBtn.setAttribute("disabled", true); //???//
+    resetBtn.style.background = "hsl(183, 64%, 25%)";
+  } else {
+    // errorMsg.style.visibility = "hidden";
+
+    if (customTipInput.value !== "") {
+      tipPercent = +customTipInput.value;
+    }
+
+    const billAmount = +billInput.value;
+    const peopleCount = +peopleInput.value;
+
+    const tip = (billAmount * tipPercent) / 100;
+    const total = billAmount + tip;
+
+    tipAmountPerPerson.textContent = `$${(tip / peopleCount).toFixed(2)}`;
+    totalAmountPerPerson.textContent = `$${(total / peopleCount).toFixed(2)}`;
+
+    resetBtn.removeAttribute("disabled");
+    resetBtn.style.background = "hsl(172, 67%, 45%)";
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  inputBill.value = inputCustomTip.value = inputPeopleCount.value = "";
+  tipAmountPerPerson.textContent = totalAmountPerPerson.textContent = "$0.00";
+  tipPercent = 0;
+  resetBtn.setAttribute("disabled", true);
+  resetBtn.style.background = "hsl(183, 64%, 25%)";
+});
